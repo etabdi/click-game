9fe2699b2896b46f0a76780624cca3d19135f5ea
etabdi <etabdi@yahoo.com>
2019-11-18 19:29:22 -0600

    app created

---
+++ src/components/container/container.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './container.css';
+import Character from '../character';
+
+// main container for each Character component
+// loops through each index in props.characters, which contains an array of character 
+// to create a new Character component for each image
+// attaches the passed down clickEvent function to each Character component
+const container = props => (
+  // loops through
+  <div
+    className={
+      props.shake
+        ? 'container d-flex flex-wrap justify-content-center shake'
+        : 'container d-flex flex-wrap justify-content-center'
+    }
+  >
+    {props.characters.map((a, i) => <Character name={a} key={i} clickEvent={props.clickEvent} />)}
+  </div>
+);
+
+export default container;
