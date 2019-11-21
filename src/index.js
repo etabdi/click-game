5ca182c3727499ddab57949613604882e954ca0f
abdu <etabdi@yahoo.com>
2019-11-20 20:44:08 -0600

    update picture order

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickyGame from './components/ClickyGame';
-import './index.css';
+
+import beth from './images/beth.jpg';
+import tweety from './images/Tweety.gif';
+import Dopy from './images/Dopy.jpg';
+import Gifer from './images/Gifer.gif';
+import Homer from './images/Homer.jpg';
+ import Pikachu from './images/Pikachu.jpg';
+import Tricky from './images/Tricky.jpg';
+import Bird from './images/Bird.jpg';
+import Steven from './images/Steven.jpg';
+import Ape from './images/Ape.jpg';
+import Squirrl from './images/Squirrl.jpg';
+import Tom from './images/Tom.jpg';
+
+const images = [
+  beth,
+  tweety,
+  Dopy,
+  Homer,
+  Gifer,
+  Pikachu,
+  Tricky,
+  Steven,
+  Bird,
+  Ape,
+  Squirrl,
+  Tom
+];
+
+export default images;
 
 ReactDOM.render(<ClickyGame />, document.getElementById('root'));
