337de14ad49d525022f4ba4884eabeaf765af1ab
abdu <etabdi@yahoo.com>
2019-11-21 20:14:39 -0600

    update footer

--- src/components/footer/footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './footer.css';
-
-const footer = () => <div className="footer text-center">
-       <p className="m-0">Click on an image to earn points, but don't click on any more than once!</p>
-</div>;
-
-export default footer;
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,1 +1,8 @@
-export { default } from './footer';
+import React from 'react';
+
+const footer = () => <div className="footer text-center"   style={{color: "orange",fontSize: "22px", height:"60px",position:"fixed",bottom: "0px"
+,marginTop:"30px",width:"100%",backgroundColor:"#000c0c"}}>
+       <p className="m-0">Click on an image to earn points, but don't click on any more than once!</p>
+</div>;
+
+export default footer;
